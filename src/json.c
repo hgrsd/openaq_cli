319632e48be99254721125e163bff41eab5341bb
Daniel <daniel@hgrsd.nl>
2019-03-08 19:26:13 +0000

    Changed JSON processing functions to extract JSON objects into structs. Added separate functions to print data from structs. Amended core.c driver functions.

--- src/json.c
+++ src/json.c
@@ -106,80 +106,122 @@ void json_extract_measurements(const char *raw_data, measurements_t *target)
     json_decref(root);
 }
 
-void json_print_cities(const char *raw_data)
+void json_extract_cities(const char *raw_data, cities_t *target)
 {
-    json_t *root, *results, *entry, *city, *locations;
+    int array_size;
+
+    json_t *root, *results, *entry, *city, *locations, *country_code;
     json_error_t error;
 
     root = json_loads(raw_data, 0, &error);
-    results = get_results(raw_data, root);
+    results = get_results(raw_data, root);    
+    array_size = json_array_size(results);
+    
+    if (target->cities_array != NULL)
+        free(target->cities_array);
+    target->cities_array = (city_t *) malloc(sizeof(city_t) * array_size);
+    init_cities(target, array_size);
     
     for (int i = 0; i < json_array_size(results); i++)
     {
         entry = json_array_get(results, i);
         city = json_object_get(entry, "city");
         locations = json_object_get(entry, "locations");
-        printf("\n%d. %s: %lld location(s).", i + 1, 
-                json_string_value(city), json_integer_value(locations));
+        country_code = json_object_get(entry, "country");
+        strcpy_s(target->cities_array[i].name, json_string_value(city), CITY_MAX);
+        strcpy_s(target->cities_array[i].country_code, json_string_value(country_code), COUNTRY_CODE_MAX);
+        target->cities_array[i].n_locations = json_integer_value(locations);
+
     }
-    printf("\n");
 
     json_decref(root);
 }
 
-void json_print_locations(const char *raw_data)
+void json_extract_locations(const char *raw_data, locations_t *target)
 {
-    json_t *root, *results, *entry, *location, *parameters, *parameter, *coordinates, *latitude, *longitude;
+    int array_size;
+    const char *parameter_string;
+
+    json_t *root, *results, *entry, *location, *parameters, *parameter, *coordinates, *latitude, *longitude, *country_code, *city;
     json_error_t error;
 
     root = json_loads(raw_data, 0, &error);
-    results = get_results(raw_data, root);
+    results = get_results(raw_data, root);    
+    array_size = json_array_size(results);
+    
+    if (target->locations_array != NULL)
+        free(target->locations_array);
+    target->locations_array = (location_t *) malloc(sizeof(location_t) * array_size);
+    init_locations(target, array_size);
    
     for (int i = 0; i < json_array_size(results); i++)
     {
         entry = json_array_get(results, i);
         location = json_object_get(entry, "location");
+        city = json_object_get(entry, "city");
+        country_code = json_object_get(entry, "country");
         parameters = json_object_get(entry, "parameters");
         coordinates = json_object_get(entry, "coordinates");
         latitude = json_object_get(coordinates, "latitude");
         longitude = json_object_get(coordinates, "longitude");
-        printf("\n\n%d.\t%s\n\tLatitude: %f\n\tLongitude: %f\n\tParameters: ", 
-                i + 1, 
-                json_string_value(location),
-                json_real_value(latitude),
-                json_real_value(longitude));
+        strcpy_s(target->locations_array[i].name, json_string_value(location), LOCATION_MAX);
+        strcpy_s(target->locations_array[i].city, json_string_value(city), CITY_MAX);
+        strcpy_s(target->locations_array[i].country_code, json_string_value(country_code), COUNTRY_CODE_MAX);
+        target->locations_array[i].latitude = json_real_value(latitude);
+        target->locations_array[i].longitude = json_real_value(longitude);
         for (int j = 0; j < json_array_size(parameters); j++)
         {
             parameter = json_array_get(parameters, j);
-            printf("%s ", json_string_value(parameter));
+            parameter_string = json_string_value(parameter);
+            if (strcmp(parameter_string, "pm25") == 0)
+                target->locations_array[i].has_pm25 = 1;
+            else if (strcmp(parameter_string, "pm10") == 0)
+                target->locations_array[i].has_pm10 = 1;
+            else if (strcmp(parameter_string, "o3") == 0)
+                target->locations_array[i].has_o3 = 1;
+            else if (strcmp(parameter_string, "so2") == 0)
+                target->locations_array[i].has_so2 = 1;
+            else if (strcmp(parameter_string, "no2") == 0)
+                target->locations_array[i].has_no2 = 1;
+            else if (strcmp(parameter_string, "co") == 0)
+                target->locations_array[i].has_co = 1;
+            else if (strcmp(parameter_string, "bc") == 0)
+                target->locations_array[i].has_bc = 1;
         }        
     }
-    printf("\n\n");
 
     json_decref(root);
 }
 
-void json_print_countries(const char *raw_data)
+void json_extract_countries(const char *raw_data, countries_t *target)
 {
-    json_t *root, *results, *entry, *country, *country_code, *cities;
+    int array_size;
+
+    json_t *root, *results, *entry, *country, *country_code, *cities, *locations;
     json_error_t error;
 
     root = json_loads(raw_data, 0, &error);
-    results = get_results(raw_data, root);
+    results = get_results(raw_data, root);    
+    array_size = json_array_size(results);
     
+    if (target->countries_array != NULL)
+        free(target->countries_array);
+    target->countries_array = (country_t *) malloc(sizeof(country_t) * array_size);
+    init_countries(target, array_size);
+
     for (int i = 0; i < json_array_size(results); i++)
     {
         entry = json_array_get(results, i);
         country = json_object_get(entry, "name");
         country_code = json_object_get(entry, "code");
         cities = json_object_get(entry, "cities");
-        printf("\n%4d. %2s - %45s (%lld cities)", 
-                i + 1, 
-                json_string_value(country_code), 
-                json_string_value(country),
-                json_integer_value(cities));
+        locations = json_object_get(entry, "locations");
+        
+        strcpy_s(target->countries_array[i].name, json_string_value(country), COUNTRY_MAX);
+        strcpy_s(target->countries_array[i].country_code, json_string_value(country_code), COUNTRY_CODE_MAX);
+        target->countries_array[i].n_cities = json_integer_value(cities);
+        target->countries_array[i].n_locations = json_integer_value(locations);
     }
-    printf("\n");
-
+    
     json_decref(root);
 }
--- src/json.h
+++ src/json.h
@@ -3,13 +3,14 @@
 
 #include "data.h"
 
-// Extracts cities from the raw string passed in and prints them to stdout.
-void json_print_cities(const char *raw_data);
-// Extracts countries from the raw string passed in and prints them to stdout.
-void json_print_countries(const char *raw_data);
-// Extracts locations from the raw string passed in and prints them to stdout.
-void json_print_locations(const char *raw_data);
 // Extracts measurements from the raw string and stores them as an array of measurement_t structs. 
 void json_extract_measurements(const char *raw_data, measurements_t *target);
+// Extracts countries from the raw string and stores them as an array of country_t structs. 
+void json_extract_countries(const char *raw_data, countries_t *target);
+// Extracts cities from the raw  string and stores them as an array of city_t structs.
+void json_extract_cities(const char *raw_data, cities_t *target);
+// Extracts locations from the raw  string and stores them as an array of location_t structs.
+void json_extract_locations(const char *raw_data, locations_t *target);
+
 
 #endif
